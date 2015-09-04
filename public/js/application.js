f8929e7fe8ff5128eca612a0548e05129f44a34d
Angel Vanegas <angelman@gmail.com>
2015-09-04 13:25:10 -0400

    new portfolio

---
+++ public/js/application.js
@@ -0,0 +1,17 @@
+var main = function(){
+$('#projects').hide();
+
+$('.about').on('click', function(){
+  $('#content').show(this);
+  $('#projects').hide();
+
+});
+
+$('.project').on('click', function(){
+  $('#projects').show(this);
+  $('#content').hide();
+});
+
+};
+
+$(main)
